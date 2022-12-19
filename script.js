1ae604707bd17c68b5de95df8996f5a71603cd66
gaurav5677 <85819394+gaurav5677@users.noreply.github.com>
2022-12-19 14:26:41 +0530

    finished

--- script.js
+++ script.js
@@ -1,42 +1,68 @@
-// using Asyncronas fetch we  will fetch random quotes
-
 const quoteContainer = document.getElementById("quote-container");
 const quoteText = document.getElementById("quote");
 const authorText = document.getElementById("author");
 const twitterBtn = document.getElementById("twitter");
 const newQuoteBtn = document.getElementById("new-quote");
 const loader = document.getElementById("loader");
-// Quotes form API
 
-async function getQuote() {
-  const proxyUrl = "https://cors-anywhere.herokuapp.com/";
-  const apiUrl =
-    "http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json";
+let apiQuotes = [];
 
-  // (  '  ?  '  ) is used to add Query String
+// Loading Spinner Shown
+function loading() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+
+// Remove Loading Spinner
+function complete() {
+  quoteContainer.hidden = false;
+  loader.hidden = true;
+}
+
+// Show New Quote
+function newQuote() {
+  loading();
+  // Pick a random quote from array
+  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+  // Check if Author field is blank and replace it with 'Unknown'
+  if (!quote.author) {
+    authorText.textContent = "Unknown";
+  } else {
+    authorText.textContent = quote.author;
+  }
+  // Check Quote length to determine styling
+  if (quote.text.length > 120) {
+    quoteText.classList.add("long-quote");
+  } else {
+    quoteText.classList.remove("long-quote");
+  }
+  // Set Quote, Hide Loader
+  quoteText.textContent = quote.text;
+  complete();
+}
+
+// Get Quotes From API
+async function getQuotes() {
+  loading();
+  const apiUrl = "https://jacintodesign.github.io/quotes-api/data/quotes.json";
   try {
-    const response = await fetch(proxyUrl + apiUrl);
-    // because we have Async this const response will not going to set until the await fetch(apiUrl) finish fetching
-    const data = await response.json();
-    // if Author is bland , add 'Unknow';
-    if (data.quoteAuthor === "") {
-      authorText.innerText = "Unknown";
-    } else {
-      authorText.innerText = data.quoteAuthor;
-    }
-    // this data will not be set until it finishes  returning const response in jspon format
-    if (data.quoteText.lenght > 120) {
-      quoteText.classList.add("long-quote");
-    } else {
-      quoteText.classList.remove("long-quote");
-    }
-
-    quoteText.innerText = data.quoteText;
+    const response = await fetch(apiUrl);
+    apiQuotes = await response.json();
+    newQuote();
   } catch (error) {
-    //  getQuote();
+    // Catch Error Here
   }
 }
 
-//  On load
+// Tweet Quote
+function tweetQuote() {
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.innerText} - ${authorText.innerText}`;
+  window.open(twitterUrl, "_blank");
+}
 
-getQuote();
+// Event Listeners
+newQuoteBtn.addEventListener("click", newQuote);
+twitterBtn.addEventListener("click", tweetQuote);
+
+// On Load
+getQuotes();
