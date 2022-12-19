d9b5a6068c2eddea5a62fc2530dba041206c9ea6
gaurav5677 <85819394+gaurav5677@users.noreply.github.com>
2022-12-19 14:11:41 +0530

    Dom manpulating

---
+++ script.js
@@ -0,0 +1,42 @@
+// using Asyncronas fetch we  will fetch random quotes
+
+const quoteContainer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const twitterBtn = document.getElementById("twitter");
+const newQuoteBtn = document.getElementById("new-quote");
+
+// Quotes form API
+
+async function getQuote() {
+  const proxyUrl = "https://cors-anywhere.herokuapp.com/";
+  const apiUrl =
+    "http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json";
+
+  // (  '  ?  '  ) is used to add Query String
+  try {
+    const response = await fetch(proxyUrl + apiUrl);
+    // because we have Async this const response will not going to set until the await fetch(apiUrl) finish fetching
+    const data = await response.json();
+    // if Author is bland , add 'Unknow';
+    if (data.quoteAuthor === "") {
+      authorText.innerText = "Unknown";
+    } else {
+      authorText.innerText = data.quoteAuthor;
+    }
+    // this data will not be set until it finishes  returning const response in jspon format
+    if (data.quoteText.lenght > 50) {
+      quoteText.classList.add("long-quote");
+    } else {
+      quoteText.classList.remove("long-quote");
+    }
+
+    quoteText.innerText = data.quoteText;
+  } catch (error) {
+    //  getQuote();
+  }
+}
+
+//  On load
+
+getQuote();
